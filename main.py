d855119f25c7aa9e15ed5ed33ea34eb2c95c22cf
amisiespoir-07 <espoirlumbu@gmail.com>
2024-02-13 10:40:25 +0800

    Downloading libraries and dependencies

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+%matplotlib inline
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+# example text for model training (sms message)
+simple_train = ['call you tonight', 'call me a cab', 'Please call me..PLEASE!']
