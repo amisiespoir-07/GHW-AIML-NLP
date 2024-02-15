117b30b1447419da007dacb61417fd326308d3f5
amisiespoir-07 <espoirlumbu@gmail.com>
2024-02-15 08:40:47 +0800

    created the vector and matrix for the vocabularly

--- main.py
+++ main.py
@@ -2,10 +2,36 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
 
-%matplotlib inline
 sns.set_style("whitegrid")
 plt.style.use("fivethirtyeight")
 
 # example text for model training (sms message)
 simple_train = ['call you tonight', 'call me a cab', 'Please call me..PLEASE!']
+
+vect = CountVectorizer()
+vect.fit(simple_train)
+print(vect.get_feature_names_out())
+
+#transform training data in a 'document-term matrix'
+simple_train_dtm = vect.transform(simple_train)
+print(simple_train_dtm)
+
+#convert sparse matrix to a dense matrix
+print(simple_train_dtm.toarray())
+
+#examine the vocabulary and document-termn matrix together
+df = pd.DataFrame(simple_train_dtm.toarray(), columns=vect.get_feature_names_out())
+print("TRAIN DATAFRAME:")
+print(df)
+
+# example test for model testing
+simple_test = ["please don't call me"]
+
+#transform testing data into a document-term matrix (using existing vocabulary)
+simple_test_dtm = vect.transform(simple_test)
+
+test_df = pd.DataFrame(simple_test_dtm.toarray(), columns=vect.get_feature_names_out())
+print('TEST DATAFRAME:')
+print(test_df)
