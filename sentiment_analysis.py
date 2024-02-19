74b27290f16b558c2646695759716eebcba8489c
amisiespoir-07 <espoirlumbu@gmail.com>
2024-02-20 01:12:57 +0800

    sentiment_analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,3 +4,18 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+
+#create p[rprocess_text function
+def preprocess_text(text):
+
+    #Tokenize the text
+    tokens = word_tokenize
+
+# download nltk corpus (first time only)
+nltk.download('all')
+
+# load the amazon review dataset 
+df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/amazon.csv')
+
+print('AMAZON REVIEW DATASET: ')
+print(df.head())    
